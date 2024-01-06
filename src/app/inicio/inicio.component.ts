e4f07479f3e7c01bc5616af8e1bea897c74c77f4
Hilbert Nunes <hilbert.nunes.dev@gmail.com>
2024-01-06 01:06:40 -0300

    feat: CRUD restrito

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Produto } from '../models/Produto.model';
+import { ProdutoService } from '../produto.service';
 
 @Component({
   selector: 'app-inicio',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent {
+  public produtos: Produto[] = [];
+  constructor(private _produtoService: ProdutoService) { }
+  ngOnInit(): void {
+    this.listarProdutos();
+  }
+  listarProdutos(): void {
+    this._produtoService.getProdutos()
+      .subscribe(
+        retornaProduto => {
+          this.produtos = retornaProduto.map(
+            item => {
+              return new Produto(
+                item.id,
+                item.produto,
+                item.descricao,
+                item.foto,
+                item.preco
+              );
+            }
+          )
+        }
+      )
+  }
 
 }
